10a6763aba767791f55371533a30d6a0e7eea2a5
Alok Singh <aloksingh@MacBook.local>
2026-05-11 00:40:42 +0530

    fix: allow all hosts in vite preview

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
-  preview: { port: process.env.PORT || 4173, host: true }
+  preview: { 
+    port: process.env.PORT || 4173, 
+    host: true,
+    allowedHosts: 'all'
+  }
 })
